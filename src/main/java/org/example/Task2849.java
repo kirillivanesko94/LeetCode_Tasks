a2521b1e0f3d10d1e8aef5757503e2af482b2f57
Kirill Ivanesko <kirill.ivanesko@yandex.ru>
2023-11-16 00:24:14 +0300

    Task 2849

---
+++ src/main/java/org/example/Task2849.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Task2849 {
+    public static void main(String[] args) {
+        int sx = 3, sy = 1, fx = 7, fy = 3, t = 3;
+        System.out.println(solution(sx, sy, fx, fy, t));
+
+    }
+
+    private static boolean solution(int sx, int sy, int fx, int fy, int t) {
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+        queue.offer(new int[]{sx, sy, 0});
+
+        while (!queue.isEmpty()) {
+            int[] currentPosition = queue.poll();
+            int x = currentPosition[0];
+            int y = currentPosition[1];
+            int currentTime = currentPosition[2];
+
+            if (x == fx && y == fy && t == currentTime) {
+                return true;
+            }
+            if (currentTime > t) {
+                continue;
+            }
+
+            for (int[] dir : directions) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+
+                queue.offer(new int[]{nx, ny, currentTime + 1});
+            }
+        }
+        return false;
+    }
+}
