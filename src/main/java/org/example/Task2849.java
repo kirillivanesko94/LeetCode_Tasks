73407d82832b7bfa69bb6c1e22a492111bd58bb0
Kirill Ivanesko <kirill.ivanesko@yandex.ru>
2023-11-16 00:25:03 +0300

    Task 2849

--- src/main/java/org/example/Task2849.java
+++ src/main/java/org/example/Task2849.java
@@ -2,7 +2,12 @@ package org.example;
 
 import java.util.LinkedList;
 import java.util.Queue;
-
+/*
+You are given four integers sx, sy, fx, fy, and a non-negative integer t.
+In an infinite 2D grid, you start at the cell (sx, sy). Each second, you must move to any of its adjacent cells.
+Return true if you can reach cell (fx, fy) after exactly t seconds, or false otherwise.
+A cell's adjacent cells are the 8 cells around it that share at least one corner with it. You can visit the same cell several times.
+ */
 public class Task2849 {
     public static void main(String[] args) {
         int sx = 3, sy = 1, fx = 7, fy = 3, t = 3;
